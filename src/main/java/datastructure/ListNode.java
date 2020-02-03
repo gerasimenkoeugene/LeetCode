f666046f7ca6ae6ba85fd977c050e63cf0b76937
igerasimenko-blackbird <igerasimenko@blackbird-lab.com>
2020-02-03 18:30:39 +0200

    changed node toString method

--- src/main/java/datastructure/ListNode.java
+++ src/main/java/datastructure/ListNode.java
@@ -10,9 +10,7 @@ public class ListNode {
 
     @Override
     public String toString() {
-        return "ListNode{" +
-                "val=" + val +
-                ", next -> " + next +
-                '}';
+        return "" + val +
+                ",  " + next;
     }
 }
