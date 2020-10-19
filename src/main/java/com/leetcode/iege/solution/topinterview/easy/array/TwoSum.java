89a864cc89ca54b91ac5d22ca3ee0312a8d5c6d5
Ievgenii Gerasimenko <ievgenii.gerasimenko@doclerholding.com>
2020-10-19 15:36:27 +0200

    two sum

---
+++ src/main/java/com/leetcode/iege/solution/topinterview/easy/array/TwoSum.java
@@ -0,0 +1,26 @@
+package com.leetcode.iege.solution.topinterview.easy.array;
+
+import com.sun.source.tree.BreakTree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> diffMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+
+            if (diffMap.containsKey(nums[i])) {
+                return new int[]{diffMap.get(nums[i]), i};
+            }
+            diffMap.put(diff, i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new TwoSum().twoSum(new int[]{2, 7, 11, 15}, 9));
+    }
+}
